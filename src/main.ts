076dfab9dc2c47d8eaf26bb86c09b447ce153350
Felipe Rossato <feliperossato91@gmail.com>
2025-11-27 10:56:32 -0300

    chore: remove express header

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.getHttpAdapter().getInstance().disable('x-powered-by')
   app.enableCors()
   await app.listen(3000)
 }
